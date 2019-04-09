676077ba32e8bfe2444566e78a79f3d2408e2fa3
Fabricio <fcaicedom28@gmail.com>
2019-04-08 21:44:52 -0500

    Se agrego el modelo de Clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cliente(models.Model):
+	client = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Cliente")
+
+	def __str__(self):
+   		return self.client.username
+
